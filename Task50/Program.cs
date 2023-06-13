a24634af35d2fdedf7ea6649ad3fd2cc199b0320
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-13 19:48:53 +0300

    completed Task50

---
+++ Task50/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите число рядов: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число столбцов: ");
+int collumns = int.Parse(Console.ReadLine()!);
+int [,] numbers = new int [rows, collumns];
+for (int i = 0; i < rows; i++)
+{
+    for (int n = 0; n < collumns; n++ )
+        {
+            numbers [i, n] = new Random().Next(-100,100);
+            Console.Write($" {numbers[i, n]} ");
+        }
+    Console.WriteLine();
+}
+Console.Write("Введите столбец необходимого числа: ");
+int CollumnSearch = int.Parse(Console.ReadLine()!);
+Console.Write("Введите ряд необходимого числа: ");
+int RowSearch = int.Parse(Console.ReadLine()!);
+if (CollumnSearch > collumns-1 || RowSearch > rows-1)
+    Console.WriteLine("Такой позиции нет");
+else
+    Console.WriteLine($"Число на данной позиции - {numbers[RowSearch,CollumnSearch]}");
